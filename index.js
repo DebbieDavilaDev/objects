b36a48bd9cf4d66c605998aaf3dea55a5cdfa672
Debbie Davila <debbie@debbiedavila.com>
2023-10-05 20:39:02 -0400

    message

---
+++ index.js
@@ -0,0 +1,21 @@
+const person = {//Declare a new object
+    firstName: "Todd",
+    age: 28
+}
+// print the person's name
+console.log(person.firstName)
+
+// add one to the person's age
+person.age++
+
+console.log(person.age)
+
+person.lastName = "Albert"
+/*
+const person = {
+    firstName:"Todd"
+    age: 29
+    lastName: "Albert"
+}
+*/
+console.log(person)
